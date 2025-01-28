2dc9126f62ac7412daba8f8d88154b6f9c7580b4
Aaradhya Sharma <Aaradhya.Sharma@sciforn.com>
2025-01-28 14:26:40 +0530

    null asset case

--- MarkscanAPI/Models/YoutubeURLs.cs
+++ MarkscanAPI/Models/YoutubeURLs.cs
@@ -30,12 +30,23 @@ namespace MarkscanAPI.Models
         public static async Task<IEnumerable<YoutubeURLs>> GetURLsForClient(IDatabaseConnection databaseConnection, string? ClientId, DateTime StartDate, DateTime? EndDate, string? AssetName)
         {
             using var conn = databaseConnection.GetConnection();
-            var assetId = await conn.QueryFirstOrDefaultAsync<string>(@"select Id from Asset where lower(AssetName)=lower(@AssetName)", new { AssetName });
-            return await conn.QueryAsync<YoutubeURLs>(@"Select i.SourceURL,A.AssetName AssetName, i.URLUploadDate, i.ViewCount, i.LikeCount, i.RemovalStatus, i.IsChannelSuspended from YoutubeURLs i
-                        inner join Asset A on A.id = i.AssetId and A.Active=1 and i.Active=1 and AssetId=@assetId
-                        join ClientMaster cl on cl.Id=A.ClientMasterId and cl.Active=1 and cl.Id=@ClientId
-                        where i.URLUploadDate >= @YTStartDate and i.URLUploadDate<= @YTEndDate and  i.IsInvalidURL = 0;"
-                        , new { ClientId, YTStartDate = StartDate.AddDays(-1).ToString("yyyy-MM-dd") + " 18:30:00", YTEndDate = EndDate?.ToString("yyyy-MM-dd") + " 18:30:00", assetId, commandTimeout = 3000 });
+            if(string.IsNullOrEmpty(AssetName))
+            {
+                return await conn.QueryAsync<YoutubeURLs>(@"Select i.SourceURL,A.AssetName AssetName, i.URLUploadDate, i.ViewCount, i.LikeCount, i.RemovalStatus, i.IsChannelSuspended from YoutubeURLs i
+                            inner join Asset A on A.id = i.AssetId and A.Active=1 and i.Active=1
+                            join ClientMaster cl on cl.Id=A.ClientMasterId and cl.Active=1 and cl.Id=@ClientId
+                            where i.URLUploadDate >= @YTStartDate and i.URLUploadDate<= @YTEndDate and  i.IsInvalidURL = 0;"
+                            , new { ClientId, YTStartDate = StartDate.AddDays(-1).ToString("yyyy-MM-dd") + " 18:30:00", YTEndDate = EndDate?.ToString("yyyy-MM-dd") + " 18:30:00", commandTimeout = 3000 });
+            }
+            else
+            {
+                var assetId = await conn.QueryFirstOrDefaultAsync<string>(@"select Id from Asset where lower(AssetName)=lower(@AssetName)", new { AssetName });
+                return await conn.QueryAsync<YoutubeURLs>(@"Select i.SourceURL,A.AssetName AssetName, i.URLUploadDate, i.ViewCount, i.LikeCount, i.RemovalStatus, i.IsChannelSuspended from YoutubeURLs i
+                            inner join Asset A on A.id = i.AssetId and A.Active=1 and i.Active=1 and AssetId=@assetId
+                            join ClientMaster cl on cl.Id=A.ClientMasterId and cl.Active=1 and cl.Id=@ClientId
+                            where i.URLUploadDate >= @YTStartDate and i.URLUploadDate<= @YTEndDate and  i.IsInvalidURL = 0;"
+                            , new { ClientId, YTStartDate = StartDate.AddDays(-1).ToString("yyyy-MM-dd") + " 18:30:00", YTEndDate = EndDate?.ToString("yyyy-MM-dd") + " 18:30:00", assetId, commandTimeout = 3000 });
+            }
         }
     }
 }
